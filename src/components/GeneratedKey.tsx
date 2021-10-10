dcedf4cd8530fae1639e3b5476c7ac9d54021148
reszi123 <imrezz07@gmail.com>
2021-10-10 22:25:08 +0300

    Update GeneratedKey.tsx

--- src/components/GeneratedKey.tsx
+++ src/components/GeneratedKey.tsx
@@ -10,35 +10,33 @@ interface IGenerated {
 const GeneratedKey: FC<IGenerated> = (props) => {
 
     const [keyBoard, setKeyBoard] = useState("")
-    const idPage: number = parseInt(JSON.parse(localStorage.getItem("idPage")!))
+    const [arrayMovie, setArrayMovie] = useState<string[]>([])
     const nameMovie = data.results[props.idpage].title
-    const indexArray: number[] = []
+    const [indexArray, setIndexArray] = useState<number[]>([])
     const arrayNameMovie = nameMovie.split(" ").join("").split("");
 
-    const changeArrayNameMovie = () => {
-        for (let i = 3; i >= 0; i--) {
+    document.addEventListener("keydown", function getKey(e) {
+        const keyArray = [e.key]
+        setKeyBoard(keyArray[0])
+        arrayMovie.push(keyArray[0])
+        console.log(indexArray.sort(function (a: any, b: any) {
+            return a - b;
+        }))
+        document.removeEventListener("keydown", getKey);
+    })
+
+    useEffect(() => {
+        for (let i = 2; i >= 0; i--) {
             const randomIndex = Math.floor(Math.random() * arrayNameMovie.length)
             arrayNameMovie.splice(randomIndex, 1, " ")
             indexArray.push(randomIndex)
+            setArrayMovie(arrayNameMovie)
         }
-        return arrayNameMovie
-    }
-    const newArrayNameMovie = changeArrayNameMovie()
-
-    useEffect(() => {
-        const getKey = (e: any) => {
-            const key = e.key
-            console.log(key)
-            setKeyBoard(key)
-            document.removeEventListener('keyup', getKey);
-        }
-
-        document.addEventListener("keyup", getKey)
     }, [])
 
     return (
         <div>
-            {newArrayNameMovie.map((letter: any) =>
+            {arrayMovie.map((letter: any) =>
                 <LettersView letters={letter} props={undefined} />
             )}
         </div>
