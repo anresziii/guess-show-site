d3c52409a604a3e2012abd13ab8e4767cc64ccd6
reszi123 <imrezz07@gmail.com>
2021-10-16 05:07:46 +0300

    Update GeneratedKey.tsx

--- src/components/GeneratedKey.tsx
+++ src/components/GeneratedKey.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useState, useMemo, useEffect } from 'react'
 import LettersView from './LettersView';
 import data from "../movie.json"
+import "../style/App.css"
 
 interface IGenerated {
     props: any
@@ -17,48 +18,64 @@ const GeneratedKey: FC<IGenerated> = (props) => {
     const [indexArray, setIndexArray] = useState<number[]>([])
     const arrayNameMovie = nameMovie.split(" ").join("").split("");
 
-    const getKey = (e: any) => {
-        const key = e.key
-        let count = 0
-        console.log(key)
-        for (let i = 65; i <= 90; i++) {
-            if (key.toUpperCase() == String.fromCharCode(i)) {
-                count = 1
-            } else if (key.toLowerCase() == "backspace") {
-                count = 10
-            }
-        }
-        console.log(count)
-        if (count == 1) {
-            indexArray.sort(function (a: any, b: any) {
-                return a - b;
-            })
-            arrayMovie.splice(indexArray[idLetter], 1, key)
-            console.log("test")
-            setIdLetter(idLetter + 1)
-        } else if (count == 10) {
-            if (idLetter >= 1) {
-                setIdLetter(idLetter - 1)
-                arrayMovie.splice(indexArray[idLetter], 1, "")
-            }
-        }
-        setKeyBoard(key)
-        document.removeEventListener("keydown", getKey);
-    }
-
-    document.addEventListener("keydown", getKey)
-
-    useEffect(() => {
+    const sortArrays = () => {
         for (let i = 3; i >= 0; i--) {
             const randomIndex = Math.floor(Math.random() * arrayNameMovie.length)
-            arrayNameMovie.splice(randomIndex, 1, " ")
-            indexArray.push(randomIndex)
+            if (randomIndex in indexArray) {
+                const randomIndex = Math.floor(Math.random() * arrayNameMovie.length)
+                arrayNameMovie.splice(randomIndex, 1, " ")
+                indexArray.push(randomIndex)
+            } else {
+                arrayNameMovie.splice(randomIndex, 1, " ")
+                indexArray.push(randomIndex)
+            }
         }
+        indexArray.sort(function (a: any, b: any) {
+            return a - b;
+        })
+        console.log(indexArray)
         setArrayMovie(arrayNameMovie)
+    }
+
+    useEffect(() => {
+        sortArrays()
     }, [])
 
+    useEffect(() => {
+        const getKey = (e: any) => {
+            const key = e.key
+            let count = 0
+            for (let i = 65; i <= 90; i++) {
+                if (key.toUpperCase() == String.fromCharCode(i)) {
+                    count = 1
+                } else if (key.toLowerCase() == "backspace") {
+                    count = 10
+                }
+            }
+
+            if (count == 1) {
+                arrayMovie.splice(indexArray[idLetter], 1, key)
+                setIdLetter(idLetter + 1)
+                console.log(idLetter)
+                console.log(indexArray[idLetter])
+            } else if (count == 10) {
+                if (idLetter >= 1) {
+                    setIdLetter(idLetter - 1)
+                    arrayMovie.splice(indexArray[idLetter], 1, "")
+                    console.log(idLetter)
+                    console.log(indexArray[idLetter])
+                }
+            }
+            setKeyBoard(key)
+        }
+        document.addEventListener("keydown", getKey)
+        return () => {
+            document.removeEventListener("keydown", getKey);
+        }
+    }, [idLetter])
+
     return (
-        <div>
+        <div className="down__word">
             {arrayMovie.map((letter: any) =>
                 <LettersView letters={letter} props={undefined} />
             )}
